a1cf997259ea1b44a16304b2cc23101db7f7dfa6
Praveen Selvaraj <praveen.beatle@gmail.com>
2017-11-09 19:24:52 +0530

    divided code into methods

--- client.py
+++ client.py
@@ -11,6 +11,7 @@ from threading import Thread
 import re
 import argparse
 import dlib
+from PIL import Image
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-ip')
@@ -20,9 +21,10 @@ parser.add_argument('-relay_port', type=int, help='relay switch input port')
 args = parser.parse_args()
 # Set relay port
 output_port = getattr(args, 'relay_port') or 25
+# Check if processig data on server
+server_process = args.ip == None ? False : True
 # Status of NC when ON aka closed
 relay_off = True
-
 # Set up dlib's face detection 
 # HOG face detector 
 face_detector = dlib.get_frontal_face_detector()
@@ -55,31 +57,53 @@ def result():
                 print('RESULT FROM THE SERVER ::::  ', result_stream)
                 for prediction in p.findall(result_stream.split('===\n')[1]):
                     if float(prediction) >= 0.80 and relay_off:
-                        relay(10)
+                        relay(5)
                     print(prediction)
     finally:
         print('result finally')
+
+def set_up_socket_connection():
+    # Get hostname from command line arguement
+    server_hostname = args.ip
+    server_port = getattr(args, 'server_port') or 8000
+    # Connect a client socket to hostname:8000 (your server)
+    client_socket = socket.socket()
+    client_socket.connect((server_hostname, server_port))
+    
+    # Make a file-like object out of the connection
+    connection = client_socket.makefile('wb')
+    # Create thread for listening to result from the server
+    thread = Thread(target=result, args=())
+    thread.daemon = True
+    # Thread start listening for eternity
+    thread.start()
+    return connection, client_socket
+
+def send_stream_data_to_server(stream, connection):
+    # Write the length of the capture to the stream and flush to
+    # ensure it was actually sent.
+    connection.write(struct.pack('<L', stream.tell()))
+    connection.flush()
+    # Rewind the stream and send the image data over the wire
+    stream.seek(0)
+    connection.write(stream.read())
+
+def end_stream(connection):
+    # Write a length of 0 to the stream to signal that we are done
+    print('sending 0 to the connection!')
+    connection.write(struct.pack('<L', 0))
 
 # Set up GPIO for LED/Relay
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(output_port, GPIO.OUT)
-# Get hostname from command line arguement
-server_hostname = args.ip
-server_port = getattr(args, 'server_port') or 8000
-# Connect a client socket to hostname:8000 (your server)
-client_socket = socket.socket()
-client_socket.connect((server_hostname, server_port))
 # Create motion sensor and accept input on port
 pir = MotionSensor(getattr(args, 'ms_port') or 17)
-# Make a file-like object out of the connection
-connection = client_socket.makefile('wb')
-# Create thread for listening to result from the server
-thread = Thread(target=result, args=())
-thread.daemon = True
+
+# If processing on a server then set up socket conenction
+if server_process:
+    connection, client_socket = set_up_socket_connection()
 try:
-    # Thread start listening for eternity
-    thread.start()
     while True:
         if pir.motion_detected:
             print('Motion Detected!')
@@ -95,22 +119,22 @@ try:
                 stream = io.BytesIO()
                 print('about to capture')
                 for c in camera.capture_continuous(stream, 'jpeg'):
-                    # Write the length of the capture to the stream and flush to
-                    # ensure it was actually sent.
-                    connection.write(struct.pack('<L', stream.tell()))
-                    connection.flush()
-                    # Rewind the stream and send the image data over the wire
-                    stream.seek(0)
-                    connection.write(stream.read())
+                    # Send stream to server
+                    if server_process:
+                        send_stream_data_to_server(stream, connection)
                     # If we've been capturing for more than 10s then quit
                     if time.time() - start > 10:
                         break
                     # Reset the stream for next capture
                     stream.seek(0)
+                    # Consctruct an image and find a face
+                    image = Image.open(stream)
+                    if find_face(image):
+                        relay(5)
                     stream.truncate()
-        	    # Write a length of 0 to the stream to signal that we are done
-                print('sending 0 to the connection!')
-                connection.write(struct.pack('<L', 0))
+
+                if server_process:
+        	       end_stream(connection)
                 print('stopping camera')
 finally:
     print('in finally')
